22c393d8361a9736fe1dcf015e420593a28920ac
lekix-lekix <pouliquen.k@gmail.com>
2025-06-24 14:47:01 +0200

    ca marche

--- wordpress/wordpress_init.sh
+++ wordpress/wordpress_init.sh
@@ -32,39 +32,39 @@ until nc -z mariadb 3306; do
 done
 
 # Télécharger et configurer WordPress
-su -s /bin/bash -c "
-    cd /var/www/html &&
-    echo 'Downloading WordPress core...' &&
-    wp core download --allow-root &&
-    echo 'Creating wp-config.php...' &&
-    wp config create --allow-root \
-        --dbname=mariadb \
-        --dbuser=user \
-        --dbpass=password \
-        --dbhost=mariadb:3306
-" www-data
+cd /var/www/html &&
+echo 'Downloading WordPress core...';
+wp core download --allow-root;
+echo 'Creating/recreating wp-config.php...';
+wp config create --allow-root \
+    --dbname=wordpress_db \
+    --dbuser=le_supervisor \
+    --dbpass=coucoutoi \
+    --dbhost=mariadb:3306 \
+    --force;
 
-# Ajouter la configuration PHP supplémentaire
-cat >> /var/www/html/wp-config.php << 'EOF'
-define('WP_DEBUG', false);
-define('WP_DEBUG_LOG', false);
-define('FORCE_SSL_ADMIN', true);
-define('WP_HOME', 'https://localhost:8080');
-define('WP_SITEURL', 'https://localhost:8080');
-EOF
+# Forcer la définition des constantes (écrase les valeurs existantes)
+echo 'Setting WordPress constants...' &&
+wp config set WP_DEBUG false --raw --allow-root &&
+wp config set WP_DEBUG_LOG false --raw --allow-root &&
+wp config set FORCE_SSL_ADMIN true --raw --allow-root &&
+wp config set WP_HOME 'https://localhost' --allow-root &&
+wp config set WP_SITEURL 'https://localhost' --allow-root
 
 # Installer WordPress
-su -s /bin/bash -c "
-    cd /var/www/html &&
-    echo 'Installing WordPress...' &&
-    wp core install --allow-root \
-        --url='https://localhost:8080' \
-        --title='C est mon site' \
-        --admin_user=supervisor \
-        --admin_password=password \
-        --admin_email=moi@coucou.com \
-        --skip-email
-" www-data
+cd /var/www/html &&
+echo 'Installing WordPress...' &&
+wp core install --allow-root \
+    --url='https://localhost/' \
+    --title='C est mon site' \
+    --admin_user=supervisor \
+    --admin_password=password \
+    --admin_email=moi@coucou.com \
+    --skip-email
+
+chown -R www-data:www-data /var/www/html
+find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html -type f -exec chmod 644 {} \;
 
 # Lancer PHP-FPM
 exec php-fpm7.4 -F
